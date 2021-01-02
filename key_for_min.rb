5207ec53b12fa38a5d4391dbb53349fd855b3d0c
Blaine R Love <blainelove94@gmail.com>
2021-01-02 21:19:25 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,7 @@ def key_for_min_value(name_hash)
   lowest = 1000
   name_hash.each do |key, value|
     if value < lowest
-      lowest = value
+      lowest = key
         puts "#{key}:"
     end
   end
