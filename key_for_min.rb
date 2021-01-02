274a873571cd6cd28e4b69385ad49d378a88d5a2
Blaine R Love <blainelove94@gmail.com>
2021-01-02 21:20:01 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,7 +2,7 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  name_lowest
+  name_lowest = ""
   name_hash.each do |key, value|
     if value < lowest
       lowest = key
